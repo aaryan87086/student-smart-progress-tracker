23ec93827d8a20d0b3f2380c839883a5c48bb0cc
Aaryan <aryandhiman540@gmail.com>
2026-02-20 15:42:19 +0530

    Initial Version of student smart progress system

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import csv
+
 #=====================================================
 #Student Smart Progress Calculator
 #Author = Aaryan
@@ -6,90 +8,106 @@
 
 print("====Student Smart Progress Calcuclator====")
 
-# Student Name 
-Name_of_Student = input("Enter the Name :")
-
-# Taking class From input user 
-student_class = int(input("Enter class From 1 to 12 :"))
-
+# ======= Functions Define =======
+def get_subjects(student_class):
 # Assign Subjects According to class 
-if (1 <= student_class <= 10):
+    if (1 <= student_class <= 10):
     # subjects for classes 1 - 10
-    subjects=["English" , "Hindi" , "Maths" , "Science" , "Computer" , "SST"]
+        return ["English" , "Hindi" , "Maths" , "Science" , "Computer" , "SST"]
 
-elif (student_class==11 or student_class==12):
+    elif (student_class==11 or student_class==12):
 
     # Ask stream for senior classes
-    stream = input("Enter the Stream (Science/Commerce) :").strip().lower()
-    if (stream == "Science"):
+        stream = input("Enter the Stream (Science/Commerce) :").strip().lower()
+        if (stream == "science"):
 
         # Ask for medical or Non Medical
-        sub_stream = input("Medical or Non-Medical? :").strip().lower()
-        if (sub_stream == "Medical"):
-            subjects = ["English" , "Hindi" , "Biology" , "Physics" , "Chemistry" , "Physical Education" ,"Music"]
-        elif (sub_stream == "Non-Medical"):
-            subjects = ["English" , "Music" , "Maths" , "Physics" , "Chemistry" , "Physical Education"]
-        else:
-            print("Program terminated due to invalid input.")
-            subjects = []   
-    elif (stream == "commerce"):
-        subjects = ["Accounts"  , "Business studies" , "Maths" , "English" , "Economics"]
+            sub_stream = input("Medical or Non-Medical? :").strip().lower()
+            if (sub_stream == "medical"):
+                return ["English" , "Hindi" , "Biology" , "Physics" , "Chemistry" , "Physical Education" ,"Music"]
+            elif (sub_stream == "non-medical"):
+                return ["English" , "Music" , "Maths" , "Physics" , "Chemistry" , "Physical Education"]
+            else:
+                print("Program terminated due to invalid input.")
+                return []   
+        elif (stream == "commerce"):
+            return ["Accounts"  , "Business studies" , "Maths" , "English" , "Economics"]
+        else :
+            print("Invalid Stream")
+            return []
     else :
-        print("Invalid Stream")
-        subjects =[]
-else :
-    print("Invalid Class")
-    subjects = []
+        print("Invalid Class")
+        return []
 
-if not subjects :
-    print("Program Stopped due to invalid input")
+def get_marks(subjects):
+   #Create empty list to store marks
+    marks_list = []
 
-#Take number of Subjects as input
-num_subjects = len(subjects)
+    #Take Marks input using loops
+    for subject in subjects:
+        score = float(input(f"Enter marks for subjects {subject} :"))
+        marks_list.append(score)
+    return marks_list
 
-#Create empty list to store marks
-marks_list = []
+def calculate_result(marks_list):
 
-#Take Marks input using loops
-for subject in subjects:
-    score = float(input(f"Enter marks for subjects {subject} :"))
-    marks_list.append(score)
-print("Marks List =" ,marks_list)
-
-#Calculating Total Marks
-total = sum(marks_list)
-
-#Calculating Percentage
-percentage = total / num_subjects
-
-#Print after ending of loop
-print("Total Marks =" , total)
-print("Percentage =" , percentage)
-
-# Find Highest and Lowest Marks
-
-highest = max(marks_list)
-lowest = min(marks_list)
-
+    #Calculating Total Marks
+    total = sum(marks_list)
+    #Calculating Percentage
+    percentage = total / len(marks_list)
+    # Find Highest and Lowest Marks
+    highest = max(marks_list)
+    lowest = min(marks_list)
 #Decide Grade based on percentage
+    if (percentage >= 90):
+        grade ="A"
+    elif (percentage >= 75):
+        grade ="B"
+    elif (percentage >= 60):
+        grade ="C"
+    elif (percentage >= 40 ):
+        grade ="D"
+    else :
+        grade ="Fail"
 
-if (percentage >= 90):
-    grade ="A"
-elif (percentage >= 75):
-    grade ="B"
-elif (percentage >= 60):
-    grade ="C"
-elif (percentage >= 40 ):
-    grade ="D"
-else :
-    grade ="Fail"
+    return total , percentage , highest , lowest , grade
+
+
+def save_data(name, student_class, total, percentage, grade):
+
+    with open("students_data.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+
+        writer.writerow([name, student_class, total, percentage, grade])
+
+
+
+# ========== Main Program ===========
+Name_of_student = input("Enter Student Name :")
+student_class = int(input("Enter class 1- 12 :"))
+
+subjects = get_subjects(student_class)
+
+if not subjects:
+    print("program stopped due to invalid input")
+    exit()
+
+marks_list = get_marks(subjects)
+
+total, percentage, highest, lowest, grade = calculate_result(marks_list)
+
+save_data(Name_of_student, student_class, total, percentage, grade)
+
+# ======= Final Input =======
 
 # Display Final Result
 print("\n ======= Result =======")
-print("Student Name =" ,Name_of_Student)
+print("Student Name =" ,Name_of_student)
 print("Marks Entered =" ,marks_list)
 print("Total Marks =" ,total)
 print("Percentage =" ,percentage)
 print("Grade = " ,grade )
 print("Highest Marks =",highest)
 print("Lowest Marks =",lowest)
+
+print("\nData saved successfully in students_data.csv")
